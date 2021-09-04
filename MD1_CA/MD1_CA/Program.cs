04723085cc47fb71bfaa81eae775e105999262d7
ArtursBund <bundulis.arturs@gmail.com>
2021-09-04 10:47:02 +0300

    Console application pievienošana

---
+++ MD1_CA/MD1_CA/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MD1_CA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
